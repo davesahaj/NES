e0dcd94db9c7231e530f428f4ca5b4590e7e1310
dave <sahajdave@gmail.com>
2021-05-28 18:16:07 +0530

    did some stuff

---
+++ include/Definition.h
@@ -0,0 +1,18 @@
+#ifndef DEFINITION_H
+#define DEFINIITON_H
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <cstring>
+#include <SFML/Graphics.hpp>
+
+using Byte = unsigned char;
+typedef std::vector<Byte> vb;
+typedef std::ostream out;
+typedef std::ofstream fout;
+typedef std::streambuf stbuff;
+#endif
