1a59c310baa9d41339df16d7a670cbf14641923e
dave <sahajdave@gmail.com>
2021-06-02 18:16:07 +0530

    did some stuff

---
+++ src/Render.cpp
@@ -0,0 +1,11 @@
+#include "Render.h"
+#include "Definition.h"
+
+namespace nes
+{
+    void Screen::create(
+        uint32_t w, uint32_t h,
+        float pixel_size, sf::Color color)
+    {
+    }
+}
